03c4ae936faab5f9fd3a3f66c5e7510fc6832802
Will Liu <will@dydx.exchange>
2023-02-16 17:17:33 -0500

    add block manager

--- types/indexer_block_events.go
+++ types/indexer_block_events.go
@@ -1,48 +1,57 @@
 package types
 
-import "cosmossdk.io/api/tendermint/abci"
-
-type (
-	// Event is a type alias for an ABCI Event
-	Event abci.Event
-
-	// Events defines a slice of Event objects
-	Events []Event
+import (
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"time"
 )
 
-type KafkaBlockEventManager {
-	blockHeight,
-	txHashes = string[],
-	time,
-	// events per transaction hash
-	events = IndexerTendermintEvent[][],
+type KafkaBlockEventManager struct {
+	block *IndexerTendermintBlock
 }
 
 func NewKafkaBlockEventManager() *KafkaBlockEventManager {
-	return &KafkaBlockEventManager{EmptyEvents()}
+	return &KafkaBlockEventManager{&IndexerTendermintBlock{}}
 }
 
-func AddEvent(event Event) {
-	eventManager.event = eventManager.event.AppendEvent(event)
+func contains[T comparable](s []T, e T) int {
+	for i, v := range s {
+		if v == e {
+			return i
+		}
+	}
+	return -1
 }
 
-func (eventManager *KafkaBlockEventManager) GetEvents() Events {
-	return eventManager.event
+// AddTransactionHash adds a transaction hash to the block event manager if this is a new transaction
+// hash. Returns the index of the new/existing transaction hash in the block event manager.
+func (eventManager *KafkaBlockEventManager) AddTransactionHash(txHash string) int {
+	if eventManager.block.TxHashes == nil {
+		eventManager.block.TxHashes = []string{}
+	}
+	if index := contains(eventManager.block.TxHashes, txHash); index != -1 {
+		return index
+	}
+	eventManager.block.TxHashes = append(eventManager.block.TxHashes, txHash)
+	eventManager.block.TxEvents = append(eventManager.block.TxEvents, &TransactionEvents{})
+	return len(eventManager.block.TxEvents) - 1
 }
 
-// prepare for sending
-
-func (eventManager *KafkaBlockEventManager) PrepareForSending() BlockEvent {
-	blockHeight,
-	txHashes,
-	time,
-	IndexerTendermintEvent[],
+func (eventManager *KafkaBlockEventManager) AddEvent(txHash string, event IndexerTendermintEvent) {
+	index := eventManager.AddTransactionHash(txHash)
+	if eventManager.block.TxEvents[index].Events == nil {
+		eventManager.block.TxEvents[index].Events = make([]*IndexerTendermintEvent, 0)
+	}
+	eventManager.block.TxEvents[index].Events = append(eventManager.block.TxEvents[index].Events, &event)
 }
 
-// protobuf
-type IndexerTendermintEvent struct {
-	//eventIndex is just the index in the txn array
-	subtype string
-	data bytes
-	// block event vs txn event? what is this? do we need this?
+func (eventManager *KafkaBlockEventManager) SetBlockHeight(blockHeight int64) {
+	eventManager.block.Height = uint32(blockHeight)
+}
+
+func (eventManager *KafkaBlockEventManager) SetBlockTime(blockTime time.Time) {
+	eventManager.block.Time = timestamppb.New(blockTime)
+}
+
+func (eventManager *KafkaBlockEventManager) GetBlock() *IndexerTendermintBlock {
+	return eventManager.block
 }
