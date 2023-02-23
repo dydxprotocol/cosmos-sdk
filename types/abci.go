9ad37b2ae7366fe933ed91be10b63e9e8cfe52ae
Will Liu <will@dydx.exchange>
2023-02-23 10:25:53 -0500

    address cmts

--- types/abci.go
+++ types/abci.go
@@ -23,8 +23,7 @@ type EndBlocker func(ctx Context, req abci.RequestEndBlock) abci.ResponseEndBloc
 // branched for the new block.
 type Commiter func(ctx Context)
 
-// Precommiter runs code during commit before the `deliverState` is reset. For the Dydx chain, it is currently
-// being used to send aggregated Indexer on-chain events to Kafka.
+// Precommiter runs code during commit before the `deliverState` is reset.
 type Precommiter func(ctx Context)
 
 // PeerFilter responds to p2p filtering queries from Tendermint
